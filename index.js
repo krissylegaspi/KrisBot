9687ed92bbc987204cecd07ea8c087c68447b7f7
Kristine <krissylegaspi@gmail.com>
2020-01-23 20:18:54 -0800

    Bot online

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
+require('dotenv').config();
 const bot = new Discord.Client();
 
 const token = process.env.DISCORD_TOKEN;
-
-require('dotenv').config();
+// console.log('token=' + token)
 
 bot.on('ready', () => {
   console.log('This bot is online!');
-})
+});
 
 bot.login(token);

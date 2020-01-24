a31e0eca849de11d405ddcb647e317d4733cbce5
Kristine <krissylegaspi@gmail.com>
2020-01-23 20:01:06 -0800

    Update token

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-require('dotenv').config();
-
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+
 const token = process.env.DISCORD_TOKEN;
+
+require('dotenv').config();
+
+bot.on('ready', () => {
+  console.log('This bot is online!');
+})
+
+bot.login(token);

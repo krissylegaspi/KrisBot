4151a696b8d04bb036823b6c16b7f25a3b9a01e0
Kristine <krissylegaspi@gmail.com>
2020-01-24 18:28:15 -0800

    Add prefix

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-const Discord = require('discord.js');
 require('dotenv').config();
+const Discord = require('discord.js');
+const {
+  Client,
+  Attachment
+} = require('discord.js');
 const bot = new Discord.Client();
-
 const token = process.env.DISCORD_TOKEN;
 // console.log('token=' + token)
+
+const PREFIX = '!';
 
 bot.on('ready', () => {
   console.log('This bot is online!');
 });
+
+bot.on('message', message => {
+
+  let args = message.content.substring(PREFIX.length).split(" ");
+
+  switch (args[0]) {
+    case 'test':
+      message.channel.sendMessage('1, 2, 3')
+      break;
+    case 'website':
+      message.channel.sendMessage('Websites can be found in #faq ')
+      break;
+    case 'meme':
+      if (args[1] === 'version') {
+        message.channel.sendMessage('');
+      } else {
+        message.channel.sendMessage('')
+      }
+      break;
+    case 'clear':
+      if (!args[1]) return message.reply('Error, please define a second argument.')
+      message.channel.bulkDelete(args[1]);
+      break;
+    case 'rip':
+      const attachment = new Attachment('./rip.png');
+      message.channel.sendMessage(attachment);
+      break;
+  }
+})
 
 bot.login(token);
